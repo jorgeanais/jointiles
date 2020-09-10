52b973fed439c4f4be8694d872e073993469e9fd
jorgeanais <jrganais@gmail.com>
2020-09-10 10:36:36 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jointiles',
+    version='0.1',
+    description='Tool to full outer join two tiles spatially',
+    author='Jorge Anais',
+    url='https://github.com/jorgeanais/jointiles',
+    author_email='jrganais@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['numpy', 'matplotlib', 'astropy', 'scipy', 'ipython'],
+    package_data={
+        '': ['*.md'],
+    },
+    zip_safe=False,
+)
